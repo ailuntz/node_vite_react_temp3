d485c1a5fb46a21f339789a3b2f635fc56412c65
“Ailuntz” <ailuntz@icloud.com>
2024-09-20 09:36:15 +0000

    Update GitHub Actions workflow with Tauri configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nodevitereacttemp3/', // 设置 `base` 路径
+  // base: '/nodevitereacttemp3/', // 设置 `base` 路径
   plugins: [react()],
 })
